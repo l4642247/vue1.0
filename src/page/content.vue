e8de973cf2a66883ca8f9ab15873c16ec38d5803
l4642247 <240467383@qq.com>
2018-09-19 17:45:29 +0800

    Initial commit

---
+++ src/page/content.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <myHeader></myHeader>
+    <!--<h2 v-text="dat.title"></h2>
+    <p v-text="dat.date"></p>
+    <hr>
+    <article v-html="dat.content"></article>-->
+    <myFooter></myFooter>
+  </div>
+</template>
+<script>
+  import myHeader from '../components/header.vue'
+  import myFooter from '../components/footer.vue'
+  export default {
+    components: { myHeader, myFooter },
+    data () {
+      return {
+        id: this.$route.params.id,
+        dat: {}
+      }
+    },
+    created () {
+      this.getData()
+    },
+    methods: {
+      getData () {
+        this.$api.get('article/content/' + this.id, null, r => {
+          this.dat = r
+          console.log(dat)
+        })
+      }
+    }
+  }
+</script>
---
+++ src/page/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <Header></Header>
+    <div class="article_list">
+      <ul>
+        <li v-for="i in list">
+          <time v-text="i.date"></time>
+          <router-link :to="'/content/' + i.id">
+            {{ i.title }}
+          </router-link>
+          <article v-text="i.summary"></article>
+        </li>
+      </ul>
+    </div>
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+  import Header from '../components/header.vue'
+  import Footer from '../components/footer.vue'
+  export default {
+    components: { Header, Footer },
+    data () {
+      return {
+        list: []
+      }
+    },
+    created () {
+      this.getData()
+    },
+    methods: {
+      getData () {
+        this.$api.get('article/all', null, r => {
+          this.list = r
+          console.log(r)
+        })
+      }
+    }
+  }
+</script>
